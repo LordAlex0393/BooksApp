1e560c6c84daf9cc5c5231a468577b2f6fa88f1e
Lord_Alex <alexstone0390@gmail.com>
2025-06-16 18:46:20 +0700

    v0.1.21 - Scroll added

--- app/src/main/java/com/example/bookapp/pages/AllReviewsScreen.kt
+++ app/src/main/java/com/example/bookapp/pages/AllReviewsScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
@@ -43,6 +45,7 @@ fun AllReviewsScreen(
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.fillMaxWidth()
+                .verticalScroll(rememberScrollState())
         ) {
             IconButton(onClick = { navController.popBackStack() }) {
                 Icon(Icons.Default.ArrowBack, contentDescription = "Назад")
--- app/src/main/java/com/example/bookapp/pages/BookListScreen.kt
+++ app/src/main/java/com/example/bookapp/pages/BookListScreen.kt
@@ -15,6 +15,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Card
@@ -67,6 +69,7 @@ fun BookListScreen(
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.fillMaxWidth()
+                .verticalScroll(rememberScrollState())
         ) {
             IconButton(onClick = { navController.popBackStack() }) {
                 Icon(Icons.Default.ArrowBack, contentDescription = "Назад")
@@ -141,7 +144,7 @@ fun BookListItem(book: Book, navController: NavController) {
             ) {
                 Text(
                     text = book.title ?: "Без названия",
-                    style = MaterialTheme.typography.titleLarge,
+                    style = MaterialTheme.typography.titleMedium,
                     maxLines = 2,
                     overflow = TextOverflow.Ellipsis
                 )
